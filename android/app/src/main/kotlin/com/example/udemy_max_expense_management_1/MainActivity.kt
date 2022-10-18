e2bb1579971da150021aaebfb3c30af9cf1622a5
carlstep <carlstep333@me.com>
2022-10-18 12:07:21 +0700

    initial code for project start

---
+++ android/app/src/main/kotlin/com/example/udemy_max_expense_management_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.udemy_max_expense_management_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Expense Management App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Xpnc'),
+      ),
+      body: Center(
+        child: Text('Widget Playground'),
+      ),
+    );
+  }
+}
